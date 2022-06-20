696cb0b9ce8bef596aec71621bf43129aefcc9fc
Pineapple <cstrikest@gmail.com>
2022-06-21 07:37:44 +0900

    学习c#。

--- Pinebot/Program.cs
+++ Pinebot/Program.cs
@@ -34,15 +34,68 @@ namespace PineBot
     }
 
 
-    internal class Program
+    interface IServerConfig
     {
+        Action open { get; set; }
+        Action close { get; set; }
+        Action<string> msg { get; set; }
+
+    }
+    class Server
+    {
+        private string ipAddress { get; }
+        public Server(string ipStr)
+        {
+            ipAddress = ipStr;
+        }
+        public void Start(Action<IServerConfig> config)
+        {
+            Console.WriteLine("Connecting to {0} ...", ipAddress);
+            
+        }
+    }
+
+    class Config : IServerConfig
+    {
+        public Action open { get; set; }
+        public Action close { get; set; }
+
+        public Action<string> msg { get; set; }
+        
+    }
+    internal class Program
+    {   
         static void Main(string[] args)
         {
+            var s = new Server("192.168.0.1");
+            s.Start(config =>
+            {
+                config.open = () =>
+                {
+                    Console.WriteLine("things while opening...");
+                };
+                config.close = (() =>
+                {
+                    Console.WriteLine("things while closing...");
+                });
+                config.msg = b =>
+                {
+                    Console.WriteLine(b);
+                };
+            });
+
+            void Fucn(IServerConfig config)
+            {
+                config.close();
+            }
+            
+            return;
+            
             Console.WriteLine("正在初始化机器人...");
             WebSocket server;
             try
             {
-                server = new WebSocket("ws://127.0.0.1.s", 8182, LogLevel.Debug);
+                server = new WebSocket("ws://127.0.0.1", 8182, LogLevel.Debug);
             }
             catch (Exception e)
             {
